78dfa907b619d17aecb2dec727c57ec2a96aafd7
christophebedard <chrisvolkoff@gmail.com>
2018-02-15 23:09:31 -0500

    Add barebones Strategy implementation

---
+++ src/decisionmaking/strategy/Strategy.cpp
@@ -0,0 +1,14 @@
+/**
+ * \file Strategy.cpp
+ * \brief asbtract strategy class implementation
+ * \author christophebedard
+ */
+
+#include "Strategy.h"
+
+Strategy::Strategy() {
+
+}
+
+Strategy::~Strategy() {
+}
